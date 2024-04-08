74681097c47e26d732bd10354859454be0f810f3
Afy Wahyu <afyww18@gmail.com>
2024-04-09 00:29:50 +0700

    upp

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,18 +1,31 @@
 
 const Herosection = () => {
+
     return (
         <section id="home">
             <div className="grid grid-cols-1 relative">
                 <div className="my-auto">
                     <div className="video-container relative">
-                        <iframe
-                            title="YouTube Video"
-                            src="https://www.youtube.com/embed/nV28eDzTs10?autoplay=1&loop=1&controls=0&mute=1"
-                            frameBorder="0"
-                            allow="autoplay; encrypted-media"
-                            allowFullScreen
-                            className="w-screen h-screen xl:w-full xl:h-screen"
-                        ></iframe>
+                        <div className="lg:hidden">
+                            <iframe
+                                title="YouTube Video"
+                                src="https://www.youtube.com/embed/ZeKwsABnfcM?autoplay=1&loop=1&controls=0&mute=1"
+                                frameBorder="0"
+                                allow="autoplay; encrypted-media"
+                                allowFullScreen
+                                className="w-screen h-screen"
+                            ></iframe>
+                        </div>
+                        <div className="hidden lg:block">
+                            <iframe
+                                title="YouTube Video"
+                                src="https://www.youtube.com/embed/nV28eDzTs10?autoplay=1&loop=1&controls=0&mute=1"
+                                frameBorder="0"
+                                allow="autoplay; encrypted-media"
+                                allowFullScreen
+                                className="w-screen h-screen"
+                            ></iframe>
+                        </div>
                     </div>
                     <div className="absolute inset-0 px-10 flex items-center justify-start">
                         <div className='w-2/3 xl:w-3/6'>
