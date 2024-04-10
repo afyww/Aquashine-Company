697d34a6ba18f13b8ade1515bedfa9c094d827cb
Afy Wahyu <afyww18@gmail.com>
2024-04-11 05:05:00 +0700

    mnb

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -4,26 +4,26 @@ function Contactus() {
     return (
         <div className="p-4 xl:p-8 bg-emerald-600 rounded-xl">
             <div className="">
-                <div className="space-y-16">
+                <div className="space-y-6">
                     <div>
-                        <h1 className="text-2xl xl:text-4xl text-white font-semibold text-left">Contact Us</h1>
+                        <h1 className="text-xl xl:text-4xl text-white font-semibold text-left">Contact Us</h1>
                     </div>
                     <div className='space-y-4'>
                         <div className='flex space-x-4'>
                             <img className='w-12 h-10' src={fb} alt="" />
-                            <h1 className='my-auto text-white'>Shinecompany</h1>
+                            <h1 className='my-auto text-sm xl:text-lg text-white'>Shinecompany</h1>
                         </div>
                         <div className='flex space-x-4'>
                             <img className='w-12 h-10' src={fb} alt="" />
-                            <h1 className='my-auto text-white'>Shinecompany</h1>
+                            <h1 className='my-auto text-sm xl:text-lg text-white'>Shinecompany</h1>
                         </div>
                         <div className='flex space-x-4'>
                             <img className='w-12 h-10' src={fb} alt="" />
-                            <h1 className='my-auto text-white'>Shinecompany</h1>
+                            <h1 className='my-auto text-sm xl:text-lg text-white'>Shinecompany</h1>
                         </div>
                         <div className='flex space-x-4'>
                             <img className='w-12 h-10' src={fb} alt="" />
-                            <h1 className='my-auto text-white'>Shinecompany</h1>
+                            <h1 className='my-auto text-sm xl:text-lg text-white'>Shinecompany</h1>
                         </div>
                     </div>
                 </div>
