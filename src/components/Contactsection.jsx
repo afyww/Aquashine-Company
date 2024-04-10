253b8a70a56d3b85276dad9a83e5cba29d2d54d0
Afy Wahyu <afyww18@gmail.com>
2024-04-10 21:40:43 +0700

    upp

--- src/components/Contactsection.jsx
+++ src/components/Contactsection.jsx
@@ -4,7 +4,7 @@ import Contactcard from "./Contactcard"
 function Contactsection() {
   return (
     <section id="contact">
-      <div className="grid grid-cols-1 bg-emerald-500 py-28 xl:py-36">
+      <div className="grid grid-cols-1 bg-gray-100 py-28 xl:py-36">
         <div className="my-auto ">
           <div className="px-10 lg:px-20 xl:px-20 2xl:px-32 ">
             <div className='w-full space-y-5 xl:space-y-24'>
