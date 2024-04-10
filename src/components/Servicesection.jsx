9674e7442700d7ed472d086db1532cb6ff9ea112
Afy Wahyu <afyww18@gmail.com>
2024-04-11 04:08:51 +0700

    up

--- src/components/Servicesection.jsx
+++ src/components/Servicesection.jsx
@@ -29,30 +29,37 @@ function Servicesection() {
                 </div>
               </motion.div>
               <motion.div {...Motiony}>
-                <div className="grid grid-cols-1 xl:grid-cols-3 gap-5 p-2 xl:p-8 rounded-2xl">
-                  <div className="space-y-10 bg-emerald-600 px-5 xl:px-10 p-3 rounded-xl">
-                    <h1 className="text-white text-2xl xl:text-4xl font-extrabold">Aquascape</h1>
-                    <p className="text-xs xl:text-sm text-extralight text-white text-left">Aquascape adalah seni menciptakan dan merancang taman air di dalam akuarium. Ini melibatkan penataan tanaman air, batu, kayu, substrat, dan elemen dekoratif lainnya secara estetis untuk menciptakan lanskap air yang menarik dan alami. Aquascape biasanya dilakukan dalam akuarium yang dirancang khusus untuk meniru lingkungan alami seperti hutan, sungai, dan danau.
-                      Dalam praktiknya, aquascaping merupakan kombinasi antara seni dan ilmu pengetahuan, di mana para aquascaper</p>
-                    <div className="">
-                      <a className="text-white text-xl xl:text-2xl font-extrabold border-white border-b-4" href="">Learn more</a>
-                    </div>
-                  </div>
-                  <div className="space-y-10 bg-white px-5 xl:px-10 p-3 rounded-xl">
-                    <h1 className="text-black text-2xl xl:text-4xl font-extrabold">Paludarium</h1>
-                    <p className="text-xs xl:text-sm text-extralight text-black text-left">
+                <div className="grid grid-cols-2 xl:grid-cols-4 gap-5 p-2 xl:p-8 rounded-2xl">
+                <div className="space-y-4 xl:space-y-10 bg-white px-5 xl:px-10 p-3 rounded-xl">
+                    <h1 className="text-black text-lg xl:text-4xl font-extrabold">Paludarium</h1>
+                    <p className="text-xs xl:text-sm text-extralight text-black line-clamp-6 text-left">
                       Paludarium adalah sebuah habitat yang merupakan perpaduan antara akuarium dan vivarium. Dalam paludarium, terdapat elemen air dan darat yang menciptakan lingkungan semi-aquatic, yang idealnya cocok untuk beberapa jenis tanaman air, reptil, amfibi, dan serangga. Biasanya, paludarium memiliki bagian air yang cukup besar untuk menyediakan ruang bagi ikan atau makhluk air lainnya, serta area daratan yang cukup luas untuk tanaman dan hewan yang lebih suka hidup di darat</p>
                     <div className="">
-                      <a className="text-black text-xl xl:text-2xl font-extrabold border-black border-b-4" href="">Learn more</a>
+                      <a className="text-black text-sm xl:text-2xl font-extrabold border-black border-b-4" href="">Learn more</a>
                     </div>
-
                   </div>
-                  <div className="space-y-10 bg-emerald-600 px-5 xl:px-10 p-3 rounded-xl">
-                    <h1 className="text-white text-2xl xl:text-4xl font-extrabold">Terrarium</h1>
-                    <p className="text-xs xl:text-sm text-extralight text-white text-left">
+                  <div className="space-y-4 xl:space-y-10 bg-emerald-600 px-5 xl:px-10 p-3 rounded-xl">
+                    <h1 className="text-white text-lg xl:text-4xl font-extrabold">Aquascape</h1>
+                    <p className="text-xs xl:text-sm text-extralight text-white line-clamp-6 text-left">Aquascape adalah seni menciptakan dan merancang taman air di dalam akuarium. Ini melibatkan penataan tanaman air, batu, kayu, substrat, dan elemen dekoratif lainnya secara estetis untuk menciptakan lanskap air yang menarik dan alami. Aquascape biasanya dilakukan dalam akuarium yang dirancang khusus untuk meniru lingkungan alami seperti hutan, sungai, dan danau.
+                      Dalam praktiknya, aquascaping merupakan kombinasi antara seni dan ilmu pengetahuan, di mana para aquascaper</p>
+                    <div className="">
+                      <a className="text-white text-sm xl:text-2xl font-extrabold border-white border-b-4" href="">Learn more</a>
+                    </div>
+                  </div>
+                  <div className="space-y-4 xl:space-y-10 bg-white px-5 xl:px-10 p-3 rounded-xl">
+                    <h1 className="text-black text-lg xl:text-4xl font-extrabold">Paludarium</h1>
+                    <p className="text-xs xl:text-sm text-extralight text-black line-clamp-6 text-left">
+                      Paludarium adalah sebuah habitat yang merupakan perpaduan antara akuarium dan vivarium. Dalam paludarium, terdapat elemen air dan darat yang menciptakan lingkungan semi-aquatic, yang idealnya cocok untuk beberapa jenis tanaman air, reptil, amfibi, dan serangga. Biasanya, paludarium memiliki bagian air yang cukup besar untuk menyediakan ruang bagi ikan atau makhluk air lainnya, serta area daratan yang cukup luas untuk tanaman dan hewan yang lebih suka hidup di darat</p>
+                    <div className="">
+                      <a className="text-black text-sm xl:text-2xl font-extrabold border-black border-b-4" href="">Learn more</a>
+                    </div>
+                  </div>
+                  <div className="space-y-4 xl:space-y-10 bg-emerald-600 px-5 xl:px-10 p-3 rounded-xl">
+                    <h1 className="text-white text-lg xl:text-4xl font-extrabold">Terrarium</h1>
+                    <p className="text-xs xl:text-sm text-extralight text-white line-clamp-6 text-left">
                       Terrarium adalah wadah transparan yang digunakan untuk menanam dan menampilkan tanaman serta kadang-kadang hewan kecil dalam lingkungan yang dikontrol. Terrarium sering kali terbuat dari kaca atau plastik yang memungkinkan cahaya masuk dan memungkinkan pengamatan tanaman atau hewan di dalamnya. Lingkungan di dalam terrarium, termasuk kelembaban, suhu, dan cahaya, dapat dikendalikan untuk menciptakan kondisi optimal.</p>
                     <div className="">
-                      <a className="text-white text-xl xl:text-2xl font-extrabold border-white border-b-4" href="">Learn more</a>
+                      <a className="text-white text-sm xl:text-2xl font-extrabold border-white border-b-4" href="">Learn more</a>
                     </div>
                   </div>
                 </div>
