e544add5b832e96cb940289e02e3c1520db36f19
Afy Wahyu <afyww18@gmail.com>
2024-04-11 20:03:35 +0700

    upp

--- src/components/Mapsection.jsx
+++ src/components/Mapsection.jsx
@@ -16,8 +16,8 @@ function Mapsection() {
                         <motion.div {...Motionx}>
                             <div>
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15841.117154466934!2d110.5018478!3d-6.9763379!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708d7fec185481%3A0xd68f4bb574c1b0d8!2sAQUASHINE%20SEMARANG!5e0!3m2!1sid!2sid!4v1712839609026!5m2!1sid!2sid"
-                                    width="700"
-                                    height="500"
+                                    width="500"
+                                    height="300"
                                     style={{ border: "0" }}
                                     allowfullscreen="1"
                                     loading="lazy"
