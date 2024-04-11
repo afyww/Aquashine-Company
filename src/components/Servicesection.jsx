d02f8850ff1981d64a0698baf8a78058b24fb19c
Afy Wahyu <afyww18@gmail.com>
2024-04-12 00:14:14 +0700

    upp

--- src/components/Servicesection.jsx
+++ src/components/Servicesection.jsx
@@ -44,7 +44,7 @@ function Servicesection() {
                       <div className='border-b-2 border-blue-400 w-full flex justify-end'>
                         <div className="">
                           <a className='text-blue-600 flex gap-2 text-sm xl:text-lg font-semibold' href="">Order Now
-                            <svg className='my-auto bg-blue-400 p-1 h-6 w-5 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='my-auto bg-blue-400 p-1 h-6 w-6 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path fillRule="evenodd" clipRule="evenodd" d="M14.2929 5.29289C14.6834 4.90237 15.3166 4.90237 15.7071 5.29289L21.7071 11.2929C22.0976 11.6834 22.0976 12.3166 21.7071 12.7071L15.7071 18.7071C15.3166 19.0976 14.6834 19.0976 14.2929 18.7071C13.9024 18.3166 13.9024 17.6834 14.2929 17.2929L18.5858 13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H18.5858L14.2929 6.70711C13.9024 6.31658 13.9024 5.68342 14.2929 5.29289Z" fill="black" />
                             </svg>
                           </a>
@@ -65,7 +65,7 @@ function Servicesection() {
                       <div className='border-b-2 border-blue-400 w-full flex justify-end'>
                         <div className="">
                           <a className='text-blue-600 flex gap-2 text-sm xl:text-lg font-semibold' href="">Order Now
-                            <svg className='my-auto bg-blue-400 p-1 h-6 w-5 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='my-auto bg-blue-400 p-1 h-6 w-6 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path fillRule="evenodd" clipRule="evenodd" d="M14.2929 5.29289C14.6834 4.90237 15.3166 4.90237 15.7071 5.29289L21.7071 11.2929C22.0976 11.6834 22.0976 12.3166 21.7071 12.7071L15.7071 18.7071C15.3166 19.0976 14.6834 19.0976 14.2929 18.7071C13.9024 18.3166 13.9024 17.6834 14.2929 17.2929L18.5858 13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H18.5858L14.2929 6.70711C13.9024 6.31658 13.9024 5.68342 14.2929 5.29289Z" fill="black" />
                             </svg>
                           </a>
@@ -86,7 +86,7 @@ function Servicesection() {
                       <div className='border-b-2 border-blue-400 w-full flex justify-end'>
                         <div className="">
                           <a className='text-blue-600 flex gap-2 text-sm xl:text-lg font-semibold' href="">Order Now
-                            <svg className='my-auto bg-blue-400 p-1 h-6 w-5 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='my-auto bg-blue-400 p-1 h-6 w-6 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path fillRule="evenodd" clipRule="evenodd" d="M14.2929 5.29289C14.6834 4.90237 15.3166 4.90237 15.7071 5.29289L21.7071 11.2929C22.0976 11.6834 22.0976 12.3166 21.7071 12.7071L15.7071 18.7071C15.3166 19.0976 14.6834 19.0976 14.2929 18.7071C13.9024 18.3166 13.9024 17.6834 14.2929 17.2929L18.5858 13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H18.5858L14.2929 6.70711C13.9024 6.31658 13.9024 5.68342 14.2929 5.29289Z" fill="black" />
                             </svg>
                           </a>
@@ -107,7 +107,7 @@ function Servicesection() {
                       <div className='border-b-2 border-blue-400 w-full flex justify-end'>
                         <div className="">
                           <a className='text-blue-600 flex gap-2 text-sm xl:text-lg font-semibold' href="">Order Now
-                            <svg className='my-auto bg-blue-400 p-1 h-6 w-5 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='my-auto bg-blue-400 p-1 h-6 w-6 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path fillRule="evenodd" clipRule="evenodd" d="M14.2929 5.29289C14.6834 4.90237 15.3166 4.90237 15.7071 5.29289L21.7071 11.2929C22.0976 11.6834 22.0976 12.3166 21.7071 12.7071L15.7071 18.7071C15.3166 19.0976 14.6834 19.0976 14.2929 18.7071C13.9024 18.3166 13.9024 17.6834 14.2929 17.2929L18.5858 13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H18.5858L14.2929 6.70711C13.9024 6.31658 13.9024 5.68342 14.2929 5.29289Z" fill="black" />
                             </svg>
                           </a>
@@ -131,7 +131,7 @@ function Servicesection() {
                       <div className='border-b-2 border-blue-400 w-full flex justify-end'>
                         <div className="">
                           <a className='text-blue-600 flex gap-2 text-sm xl:text-lg font-semibold' href="">Order Now
-                            <svg className='my-auto bg-blue-400 p-1 h-6 w-5 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='my-auto bg-blue-400 p-1 h-6 w-6 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path fillRule="evenodd" clipRule="evenodd" d="M14.2929 5.29289C14.6834 4.90237 15.3166 4.90237 15.7071 5.29289L21.7071 11.2929C22.0976 11.6834 22.0976 12.3166 21.7071 12.7071L15.7071 18.7071C15.3166 19.0976 14.6834 19.0976 14.2929 18.7071C13.9024 18.3166 13.9024 17.6834 14.2929 17.2929L18.5858 13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H18.5858L14.2929 6.70711C13.9024 6.31658 13.9024 5.68342 14.2929 5.29289Z" fill="black" />
                             </svg>
                           </a>
@@ -152,7 +152,7 @@ function Servicesection() {
                       <div className='border-b-2 border-blue-400 w-full flex justify-end'>
                         <div className="">
                           <a className='text-blue-600 flex gap-2 text-sm xl:text-lg font-semibold' href="">Order Now
-                            <svg className='my-auto bg-blue-400 p-1 h-6 w-5 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='my-auto bg-blue-400 p-1 h-6 w-6 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path fillRule="evenodd" clipRule="evenodd" d="M14.2929 5.29289C14.6834 4.90237 15.3166 4.90237 15.7071 5.29289L21.7071 11.2929C22.0976 11.6834 22.0976 12.3166 21.7071 12.7071L15.7071 18.7071C15.3166 19.0976 14.6834 19.0976 14.2929 18.7071C13.9024 18.3166 13.9024 17.6834 14.2929 17.2929L18.5858 13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H18.5858L14.2929 6.70711C13.9024 6.31658 13.9024 5.68342 14.2929 5.29289Z" fill="black" />
                             </svg>
                           </a>
@@ -173,7 +173,7 @@ function Servicesection() {
                       <div className='border-b-2 border-blue-400 w-full flex justify-end'>
                         <div className="">
                           <a className='text-blue-600 flex gap-2 text-sm xl:text-lg font-semibold' href="">Order Now
-                            <svg className='my-auto bg-blue-400 p-1 h-6 w-5 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='my-auto bg-blue-400 p-1 h-6 w-6 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path fillRule="evenodd" clipRule="evenodd" d="M14.2929 5.29289C14.6834 4.90237 15.3166 4.90237 15.7071 5.29289L21.7071 11.2929C22.0976 11.6834 22.0976 12.3166 21.7071 12.7071L15.7071 18.7071C15.3166 19.0976 14.6834 19.0976 14.2929 18.7071C13.9024 18.3166 13.9024 17.6834 14.2929 17.2929L18.5858 13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H18.5858L14.2929 6.70711C13.9024 6.31658 13.9024 5.68342 14.2929 5.29289Z" fill="black" />
                             </svg>
                           </a>
@@ -194,7 +194,7 @@ function Servicesection() {
                       <div className='border-b-2 border-blue-400 w-full flex justify-end'>
                         <div className="">
                           <a className='text-blue-600 flex gap-2 text-sm xl:text-lg font-semibold' href="">Order Now
-                            <svg className='my-auto bg-blue-400 p-1 h-6 w-5 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='my-auto bg-blue-400 p-1 h-6 w-6 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path fillRule="evenodd" clipRule="evenodd" d="M14.2929 5.29289C14.6834 4.90237 15.3166 4.90237 15.7071 5.29289L21.7071 11.2929C22.0976 11.6834 22.0976 12.3166 21.7071 12.7071L15.7071 18.7071C15.3166 19.0976 14.6834 19.0976 14.2929 18.7071C13.9024 18.3166 13.9024 17.6834 14.2929 17.2929L18.5858 13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H18.5858L14.2929 6.70711C13.9024 6.31658 13.9024 5.68342 14.2929 5.29289Z" fill="black" />
                             </svg>
                           </a>
