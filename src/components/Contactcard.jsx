ef2d725de127d05031851f55d3f53b55dd57e0b6
Afy Wahyu <afyww18@gmail.com>
2024-04-12 01:39:04 +0700

    up

--- src/components/Contactcard.jsx
+++ src/components/Contactcard.jsx
@@ -103,12 +103,15 @@ function Contactcard() {
                             placeholder="Write your message..."
                         ></textarea>
                     </div>
-                    <div>
+                    <div className="border-b-2 border-emerald-400 w-fit">
                         <button
                             type="submit"
-                            className="text-sm xl:text-xl text-emerald-600 border-emerald-400 border-b-2 w-full p-2"
-                        >
-                            Send Message on WhatsApp
+                            className="text-sm flex gap-2 xl:text-xl text-emerald-600"
+
+                        >Send Message on WhatsApp
+                            <svg className='my-auto bg-emerald-400 p-1 h-6 w-6 xl:w-7 xl:h-8' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M14.2929 5.29289C14.6834 4.90237 15.3166 4.90237 15.7071 5.29289L21.7071 11.2929C22.0976 11.6834 22.0976 12.3166 21.7071 12.7071L15.7071 18.7071C15.3166 19.0976 14.6834 19.0976 14.2929 18.7071C13.9024 18.3166 13.9024 17.6834 14.2929 17.2929L18.5858 13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H18.5858L14.2929 6.70711C13.9024 6.31658 13.9024 5.68342 14.2929 5.29289Z" fill="black" />
+                            </svg>
                         </button>
                     </div>
                 </div>
