f809b10eeb371712aeb7fcdbaf098df723dd105f
Afy Wahyu <afyww18@gmail.com>
2024-04-18 21:52:45 +0700

    upp

--- src/components/Imgsection.jsx
+++ src/components/Imgsection.jsx
@@ -5,25 +5,20 @@ import service4 from "../assets/images/service1.jpg";
 import service5 from "../assets/images/service3.jpg";
 import service6 from "../assets/images/service6.jpg";
 import service7 from "../assets/images/service7.jpg";
-import service8 from "../assets/images/service8.jpg";
 
 function Imgsection() {
     return (
         <div className="h-fit">
-            <div className="mx-auto space-y-8 overflow-auto">
+            <div className="space-y-8 overflow-auto">
                 <div className="flex space-x-4">
-                    <img className="h-1/2 w-1/2 xl:h-fit xl:w-1/2 rounded-l-xl" src={service3} alt="" />
-                    <img className="h-1/2 w-1/4 xl:h-fit xl:w-1/4" src={service4} alt="" />
-                    <img className="h-1/2 w-1/5 xl:h-fit xl:w-1/5 my-auto" src={service8} alt="" />
-                    <img className="h-1/2 w-1/3 xl:h-fit xl:w-1/3 rounded-r-xl" src={service2} alt="" />
+                    <img className="h-full w-3/4 my-auto" src={service3} alt="" />
+                    <img className="h-full w-1/2 my-auto" src={service4} alt="" />
+                    <img className="h-full w-1/2 my-auto" src={service2} alt="" />
+                    <img className="h-full w-1/2 my-auto" src={service1} alt="" />
+                    <img className="h-full w-1/3 my-auto" src={service7} alt="" />
+                    <img className="h-full w-1/3 my-auto" src={service6} alt="" />
+                    <img className="h-full w-1/3 my-auto" src={service5} alt="" />
                 </div>
-                <div className="flex space-x-4">
-                    <img className="h-1/2 w-1/2 xl:h-fit xl:w-1/2 rounded-l-xl my-auto" src={service1} alt="" />
-                    <img className="h-1/2 w-1/4 xl:h-fit xl:w-1/4" src={service7} alt="" />
-                    <img className="h-1/2 w-1/4 xl:h-fit xl:w-1/4 my-auto" src={service6} alt="" />
-                    <img className="h-1/2 w-1/3 xl:h-fit xl:w-1/3 rounded-r-xl" src={service5} alt="" />
-                </div>
-
             </div>
         </div>
     );
