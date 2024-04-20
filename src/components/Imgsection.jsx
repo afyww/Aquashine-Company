fdf0c787e25434778db710c28fc73afaf2797cac
Afy Wahyu <afyww18@gmail.com>
2024-04-21 01:35:10 +0700

    upp

--- src/components/Imgsection.jsx
+++ src/components/Imgsection.jsx
@@ -1,4 +1,10 @@
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs"; // Import arrow icons
+
+// Import images
 import service1 from "../assets/images/service4.jpg";
 import service2 from "../assets/images/service2.png";
 import service3 from "../assets/images/service5.jpg";
@@ -9,52 +15,69 @@ import service7 from "../assets/images/service7.jpg";
 import service8 from "../assets/images/service8.jpg";
 
 function Imgsection() {
-    const containerRef = useRef(null);
+    const sliderRef = useRef(null); // Create a ref for the slider component
 
-    useEffect(() => {
-        const container = containerRef.current;
-        let scrollInterval;
-
-        const startScrolling = () => {
-            scrollInterval = setInterval(() => {
-                container.scrollLeft += 2; // Adjust the scroll speed as needed
-                if (container.scrollLeft >= container.scrollWidth / 2) {
-                    container.scrollLeft = 0;
-                }
-            }, 50); // Adjust the interval as needed
-        };
-
-        const stopScrolling = () => {
-            clearInterval(scrollInterval);
-        };
-
-        container.addEventListener("mouseenter", stopScrolling);
-        container.addEventListener("mouseleave", startScrolling);
-
-        startScrolling();
-
-        return () => {
-            container.removeEventListener("mouseenter", stopScrolling);
-            container.removeEventListener("mouseleave", startScrolling);
-            clearInterval(scrollInterval);
-        };
-    }, []);
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
 
     return (
         <div className="h-fit">
-            <div
-                ref={containerRef}
-                className="mx-auto flex gap-4 overflow-hidden"
-                style={{ whiteSpace: "nowrap", overflowX: "scroll" }}
-            >
-                <img className="h-1/2 w-1/2 xl:h-fit xl:w-1/2 my-auto" src={service3} alt="" />
-                <img className="h-1/2 w-1/4 xl:h-fit xl:w-1/4 my-auto" src={service4} alt="" />
-                <img className="h-1/2 w-1/5 xl:h-fit xl:w-1/5 my-auto" src={service8} alt="" />
-                <img className="h-1/2 w-1/3 xl:h-fit xl:w-1/3 my-auto" src={service2} alt="" />
-                <img className="h-1/2 w-1/2 xl:h-fit xl:w-1/2 my-auto" src={service1} alt="" />
-                <img className="h-1/2 w-1/4 xl:h-fit xl:w-1/4 my-auto" src={service7} alt="" />
-                <img className="h-1/2 w-1/4 xl:h-fit xl:w-1/4 my-auto" src={service6} alt="" />
-                <img className="h-1/2 w-1/3 xl:h-fit xl:w-1/3 my-auto" src={service5} alt="" />
+            <div>
+                <Slider ref={sliderRef} {...settings}>
+                    <img className="h-80 w-auto p-2" src={service3} alt="" />
+                    <img className="h-80 w-auto p-2" src={service4} alt="" />
+                    <img className="h-80 w-auto p-2" src={service8} alt="" />
+                    <img className="h-80 w-auto p-2" src={service2} alt="" />
+                    <img className="h-80 w-auto p-2" src={service1} alt="" />
+                    <img className="h-80 w-auto p-2" src={service7} alt="" />
+                    <img className="h-80 w-auto p-2" src={service6} alt="" />
+                    <img className="h-80 w-auto p-2" src={service5} alt="" />
+                </Slider>
+                {/* Previous and Next buttons */}
+                <div className="my-10 flex">
+                    <button className="bg-black text-white mx-auto p-2 rounded-full slick-arrow" onClick={() => sliderRef.current.slickPrev()}>
+                        <BsArrowLeft
+                            className="w-8 h-8"
+                        />
+                    </button>
+                    <button className="bg-black text-white mx-auto p-2 rounded-full slick-arrow" onClick={() => sliderRef.current.slickNext()}>
+                        <BsArrowRight
+                            className="w-8 h-8"
+                        />
+                    </button>
+                </div>
             </div>
         </div>
     );
